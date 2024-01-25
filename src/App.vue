893109eb86da42866e2ddb82973ccbf3940ac2b5
ho-beom-cheon <cjs414ghqja@gmail.com>
2024-01-25 19:59:14 +0900

    19. props

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,4 @@
 <template>
-  <!-- v-if 와 v-show 
-    v-if는 일반적으로 토글하는데 비용이 많이 든다
-    v-if의 조건이 런타임 동안에 거의 바뀌지 않을 경우에 사용하면 된다.
-    v-show는 두개의 div를 전부 렌더링 해야해서 초기 렌더링 비용이 많이 든다.
-  -->
-  <!-- <div v-show="toggle">true</div>
-  <div v-show="!toggle">false</div> -->
-  <!-- <div v-if="toggle">true</div>
-  <div v-else>false</div> -->
-  <!-- <button @click="onToggle">Toggle</button> -->
-
   <div class="container">
     <h2>To-Do List</h2>
     <TodoSimpleForm @add-todo="addTodo"/>
@@ -17,43 +6,20 @@
     <div v-if="!todos.length">
       추가된 ToDo가 없습니다.
     </div>
-    <div
-      v-for="(todo, index) in todos"
-      :key="todo.id"
-      class="card mt-2"
-      >
-      <div class="card-body p-2 d-flex align-items-center">
-        <div class="form-check flex-grow-1">
-          <input 
-            class="form-check-input" 
-            type="checkbox"
-            v-model="todo.completed"
-          >
-          <label 
-            class="form-check-labal" 
-            :class="{ todo : todo.completed }"
-          >
-            {{ todo.subject }}
-          </label>
-        </div>
-        <div>
-          <button 
-            class="btn btn-danger btn-sm"
-            @click="deleteTodo(index)"
-          >Delete</button>
-        </div>  
-      </div>
-    </div>
+
+    <TodoList :todos="todos"/>
   </div>
 </template>
 
 <script>
 import {ref} from 'vue';
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
+import TodoList from './components/TodoList.vue';
 
 export default {
   components : {
-    TodoSimpleForm
+    TodoSimpleForm,
+    TodoList
   },
   setup() {
     const toggle = ref(false);
@@ -71,9 +37,8 @@ export default {
       todos.value.push(todo);
     }
 
-
     const deleteTodo = (index) => {
-       todos.value.splice(index, 1);
+      todos.value.splice(index, 1);
     }
 
     const onToggle = () => {
