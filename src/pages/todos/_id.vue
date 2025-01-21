725ed09a49d2dc0b73ae4158753ecd782f929564
ho-beom-cheon <cjs414ghqja@gmail.com>
2025-01-21 11:05:09 +0900

    45. ToDo 수정 페이지 route 추가

---
+++ src/pages/todos/_id.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>TodoComponent</div>
+</template>
+
+<script>
+    import { useRoute } from 'vue-router'; 
+
+    export default {
+        setup() {
+            const route = useRoute();
+            
+            console.log("route : " + route.name);
+        }
+    }
+
+</script>
+
+<style>
+</style>
