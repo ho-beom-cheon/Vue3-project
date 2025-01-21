f619bd5985356e06db91cf1d0fa778c4c2159908
ho-beom-cheon <cjs414ghqja@gmail.com>
2025-01-21 13:49:24 +0900

    46. useRouter로 페이지 이동

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,7 +4,10 @@
         :key="todo.id"
         class="card mt-2"
     >
-        <div class="card-body p-2 d-flex align-items-center">
+        <div 
+            class="card-body p-2 d-flex align-items-center"
+            @click="moveToPage(todo.id)"
+        >
         <div class="form-check flex-grow-1">
             <input 
             class="form-check-input" 
@@ -30,6 +33,7 @@
 </template>
 
 <script>
+import { useRouter } from 'vue-router';
 export default {
     // props : 부모 -> 자식
     props: {
@@ -40,6 +44,8 @@ export default {
     },
     emits: ['toggle-todo', 'delete-todo'],
     setup(props, { emit }) {
+        const router = useRouter();
+
         const toggleTodo = (index) => {
             emit('toggle-todo', index);
         }
@@ -48,9 +54,20 @@ export default {
             emit('delete-todo', index);
         }
 
+        const moveToPage = (todoId) => {
+            // router.push('todos' + todoId);
+            router.push({
+                name: 'Todo',
+                params: {
+                    id: todoId
+                }
+            });
+        }
+
         return {
             toggleTodo,
             deleteTodo,
+            moveToPage,
         }
     }
 }
