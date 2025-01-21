5a53e68b68bebc7ad9d42524ff7e0282ea1fc654
ho-beom-cheon <cjs414ghqja@gmail.com>
2025-01-21 16:07:41 +0900

    49. To-Do 페이지 컴포넌트 만들기

--- src/pages/todos/_id.vue
+++ src/pages/todos/_id.vue
@@ -1,18 +1,45 @@
 <template>
-    <div>TodoComponent</div>
+    <h1>To-Do Page</h1>
+    <div v-if="loding">
+        Loding...
+    </div>
+    <form v-else>
+        <div class="form-group">
+            <label>Todo Subject</label>
+            <input type="text" class="form-control">
+        </div>
+        <button class="btn btn-primary">
+            Save
+        </button>
+    </form>
 </template>
 
 <script>
     import { useRoute } from 'vue-router'; 
+    import axios from 'axios';
+    import { ref } from 'vue';
 
     export default {
         setup() {
             const route = useRoute();
-            
-            console.log("route : " + route.name);
+            const todo = ref(null);
+            const loding = ref(true);
+
+            const getTodo = async () => {
+                const res = await axios.get('http://localhost:3000/todos/' + route.params.id);
+                console.log(route.params.id);
+                console.log(res);
+
+                todo.value = res.data;
+                loding.value = false;
+            };
+            getTodo()
+
         }
+        
     }
 
+    
 </script>
 
 <style>
